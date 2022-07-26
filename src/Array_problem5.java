9a5941d9ecc14186757797485a26ec9a30134231
Pravin Varpe <pravinvarpe14@gmail.com>
2022-07-26 18:50:03 +0530

    added even number problem no. 5 of array

---
+++ src/Array_problem5.java
@@ -0,0 +1,23 @@
+
+public class Array_problem5 {
+
+	public static void main(String[] args) {
+		/*
+		 * int type arr referance array
+		 */
+		int [] arr = new int [] {1,2,3,4,5,6,7,8,9,10,11,12}; 
+		 
+
+		 /*
+		  * using for loop
+		  */
+		  for (int i = 1; i < arr.length; i=i+2) {  
+	            
+	       
+	        
+	        System.out.println( arr[i]);
+	}
+
+	}
+
+}
